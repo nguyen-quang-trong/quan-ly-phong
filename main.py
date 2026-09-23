152d4cc9b23a6707d896fa9cd19bce434d243f24
Nguyễn Quang Trọng <nqtrong10@gmail.com>
2026-09-23 10:55:17 +0700

    Thêm hàm lưu và đọc cơ sở dữ liệu

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from datetime import datetime
 conn=sqlite3.connect("data.db")
 cursor=conn.cursor()
 
-cursor.execute("""
-    DROP TABLE thongtinmuonphong
-""")
+# cursor.execute("""
+#     DROP TABLE thongtinmuonphong
+# """)
 
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS thongtinmuonphong(
@@ -65,21 +65,56 @@ comboGioKT.grid(row=4,column=1)
 comboPhutKT=Combobox(root,values=phut,width=3,state="readonly")
 comboPhutKT.grid(row=4,column=2)
 
-btnLuu=Button(root,text="Lưu")
+def luu():
+    hoten=entryHoTen.get()
+    mssv=entryMSSV.get()
+    ngay=comboNgay.get()
+    thang=comboThang.get()
+    nam=comboNam.get()
+    gioBD=comboGioBD.get()
+    phutBD=comboPhutBD.get()
+    gioKT=comboGioKT.get()
+    phutKT=comboPhutKT.get()
+    ngay=f"{nam}-{thang}-{ngay}"
+    giobd=f"{gioBD}:{phutBD}"
+    giokt=f"{gioKT}:{phutKT}"
+
+    cursor.execute("""
+        INSERT INTO thongtinmuonphong
+        (hoten,mssv,ngay,giobd,giokt,trangthai)
+        VALUES (?,?,?,?,?,?)
+    """,(hoten,mssv,ngay,giobd,giokt,"Đã đăng ký"))
+
+    conn.commit()
+    docDuLieu()
+
+btnLuu=Button(root,text="Lưu",command=luu)
 btnLuu.grid(row=5,column=0)
 
 lblDanhSach=Label(root,text="DANH SÁCH MƯỢN PHÒNG")
 lblDanhSach.grid(row=6,column=0,columnspan=5)
-lblSTT=Label(root,text="STT")
-lblSTT.grid(row=7,column=0)
-lblHoTenDS=Label(root,text="Họ và tên")
-lblHoTenDS.grid(row=7,column=1)
-lblMSSVDS=Label(root,text="MSSV")
-lblMSSVDS.grid(row=7,column=2)
-lblThoiGianDS=Label(root,text="Thời gian")
-lblThoiGianDS.grid(row=7,column=3)
-lblTrangThaiDS=Label(root,text="Trạng thái")
-lblTrangThaiDS.grid(row=7,column=5)
+
+bang=Treeview(root,columns=("id","hoten","mssv","thoigian","trangthai"),show="headings")
+bang.heading("id",text="STT")
+bang.heading("hoten",text="Họ và tên")
+bang.heading("mssv",text="MSSV")
+bang.heading("thoigian",text="Thời gian")
+bang.heading("trangthai",text="Trạng thái")
+bang.grid(row=7,column=0,columnspan=5)
+
+def docDuLieu():
+    cursor.execute("""
+        SELECT id,hoten,mssv,ngay,giobd,giokt,trangthai
+        FROM thongtinmuonphong
+    """)
+
+    duLieu=cursor.fetchall()
+    for dong in duLieu:
+        id,hoten,mssv,ngay,giobd,giokt,trangthai=dong
+        thoigian=ngay+" " +giobd+" - "+giokt
+        bang.insert("","end",values=(id,hoten,mssv,thoigian,trangthai))
+
+docDuLieu()
 
 lblThongKe=Label(root,text="TUẦN NÀY: 0 LƯỢT ĐĂNG KÝ | 0 NGƯỜI")
 lblThongKe.grid(row=8,column=0,columnspan=6)
